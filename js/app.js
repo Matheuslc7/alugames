45b0fb8111b0934111183dd6606e87cd86962b82
Matheus Cândido <matheus.lcandido7@gmail.com>
2023-12-17 15:45:04 -0300

    alert

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function alterarStatus (id) {
+    let jogoClicado = document.getElementById(`game-${id}`);
+    let imagem = jogoClicado.querySelector('dashboard__item__img');
+    let botao = jogoClicado.querySelector('dashboard__item__button');
+    let nomeJogo = jogoClicado.querySelector('.dashboard__item__name');
+
+    alert(nomeJogo.textContent);
+    
+}
