b088cfd9f871cc0c2d2faa210e78233d42e8a1bb
Matheus Cândido <matheus.lcandido7@gmail.com>
2023-12-17 16:00:07 -0300

    finalização

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
-function alterarStatus (id) {
-    let jogoClicado = document.getElementById(`game-${id}`);
-    let imagem = jogoClicado.querySelector('dashboard__item__img');
-    let botao = jogoClicado.querySelector('dashboard__item__button');
-    let nomeJogo = jogoClicado.querySelector('.dashboard__item__name');
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 
-    alert(nomeJogo.textContent);
-    
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        }
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
 }
